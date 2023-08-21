ec929b448fd719758083bd9909803c8ec86139e7
CleidsonGoes <jorgegoes4@gmail.com>
2023-08-21 17:56:12 -0300

    criação do comp. Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer data-testid="footer">
+      <img src="drinkIcon.svg" alt="" data-testid="drinks-bottom-btn" />
+      <img src="mealIcon.svg" alt="" data-testid="meals-bottom-btn" />
+    </footer>
+  );
+}
+
+export default Footer;
