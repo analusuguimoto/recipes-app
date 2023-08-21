fe8ec27ac979d9575c95cff1b44a954066126b82
CleidsonGoes <jorgegoes4@gmail.com>
2023-08-21 20:11:11 -0300

    group-7-req-16-a-18-feitos-footer-fixado/renderizado

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,20 @@ import '../footer.css';
 function Footer() {
   return (
     <footer data-testid="footer">
-      <img src="drinkIcon.svg" alt="drink" data-testid="drinks-bottom-btn" />
-      <img src="mealIcon.svg" alt="meal" data-testid="meals-bottom-btn" />
+      <a href="/drinks">
+        <img
+          src="/src/images/drinkIcon.svg"
+          alt="Bebida"
+          data-testid="drinks-bottom-btn"
+        />
+      </a>
+      <a href="/meals">
+        <img
+          src="/src/images/mealIcon.svg"
+          alt="Comida"
+          data-testid="meals-bottom-btn"
+        />
+      </a>
     </footer>
   );
 }
