c06a241d4f3799c8fac590f19a1970d2011ab698
Diego Gava <diego.gava15@gmail.com>
2023-08-21 12:01:04 -0300

    adicionando searchBar ao header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,23 +5,24 @@ import SearchBar from './SearchBar';
 
 function Header() {
   return (
-    <header>
-      <img
-        src={ profileIcon }
-        alt="ícone de perfil"
-        data-testid="profile-top-btn"
-      />
-      <img
-        src={ searchIcon }
-        alt="ícone de pesquisa"
-        data-testid="search-top-btn"
-      />
-      <h1>Título</h1>
+    <>
+      <header>
+        <img
+          src={ profileIcon }
+          alt="ícone de perfil"
+          data-testid="profile-top-btn"
+        />
+        <img
+          src={ searchIcon }
+          alt="ícone de pesquisa"
+          data-testid="search-top-btn"
+        />
+        <h1>Título</h1>
+
+      </header>
       <SearchBar />
-      {searchResults && searchResults.meals && searchResults.meals.length > 0 && (
-        <RecipeList recipes={ searchResults.meals } />
-      )}
-    </header>
+
+    </>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import { RecipeProvider } from './context/search-results-context';
 
 ReactDOM
   .createRoot(document.getElementById('root') as HTMLElement)
   .render(
     <BrowserRouter>
-      <App />
+      <RecipeProvider>
+        <App />
+      </RecipeProvider>
     </BrowserRouter>,
   );
