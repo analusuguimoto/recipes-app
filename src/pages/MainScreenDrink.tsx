9e7ad5a82983098dd45cdad67424bbf14fd61dd9
Igor Righi <cruigor@gmail.com>
2023-08-23 17:35:54 -0300

    criando a pag receitas in progress e add share btn e like btn

--- src/pages/MainScreenDrink.tsx
+++ src/pages/MainScreenDrink.tsx
@@ -3,37 +3,66 @@ import { useParams } from 'react-router-dom';
 import { ID_DRINKS_LINK } from '../helpers/links';
 import { fetchApi } from '../helpers/fetchApi';
 import { DrinkType } from '../types';
+import shareBtn from '../images/shareBtn.svg';
+import likeBtn from '../images/likeBtn.svg';
 
 function MainScreenDrink() {
   const { id } = useParams<{ id: string }>();
-  const [recipe, setRecipe] = useState<DrinkType>();
+  const [drinkRecipe, setDrinkRecipe] = useState<DrinkType>();
+  const [linkCopied, setLinkCopied] = useState(false);
+  const currentUrl = window.location.href;
 
   const fetchRecipe = async () => {
     const response = await fetchApi(`${ID_DRINKS_LINK}${id}`);
-    setRecipe(response.drinks[0]);
-    console.log(response);
+    setDrinkRecipe(response.drinks[0]);
   };
+
   useEffect(() => {
     fetchRecipe();
   }, []);
 
+  const handleShareBtn = () => {
+    navigator.clipboard.writeText(currentUrl)
+      .then(() => {
+        setLinkCopied(true);
+      })
+      .catch((err) => console.error('Erro ao copiar: ', err));
+  };
+
   return (
     <>
-      <h1 data-testid="recipe-category">{ recipe?.strCategory }</h1>
-      <h2 data-testid="recipe-title">{ recipe?.strDrink }</h2>
+      <nav>
+        <h1>{ drinkRecipe?.strCategory }</h1>
+        <div>
+          <button
+            data-testid="share-btn"
+            onClick={ () => handleShareBtn() }
+          >
+            { !linkCopied ? <img src={ shareBtn } alt="Botão de Compartilhamento" />
+              : <span>Link copied!</span>}
+          </button>
+          <button
+            data-testid="favorite-btn"
+          >
+            <img src={ likeBtn } alt="Botão de dar Like em uma receita" />
+          </button>
+        </div>
+      </nav>
+      <h2 data-testid="recipe-title">{ drinkRecipe?.strDrink }</h2>
       <img
         data-testid="recipe-photo"
-        src={ recipe?.strDrinkThumb }
-        alt={ recipe?.strDrink }
+        src={ drinkRecipe?.strDrinkThumb }
+        alt={ drinkRecipe?.strDrink }
         style={ { maxWidth: '200px', height: 'auto' } }
       />
+      <p data-testid="recipe-category">{ drinkRecipe?.strAlcoholic }</p>
       <div>
         <h3>Ingredients</h3>
 
       </div>
       <div>
         <h3>Instructions</h3>
-        <p data-testid="instructions">{ recipe?.strInstructions }</p>
+        <p data-testid="instructions">{ drinkRecipe?.strInstructions }</p>
       </div>
     </>
   );
--- src/pages/MainScreenFood.tsx
+++ src/pages/MainScreenFood.tsx
@@ -1,30 +1,59 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchApi } from '../helpers/fetchApi';
 import { ID_MEALS_LINK } from '../helpers/links';
+import { useRecipeContext } from '../context/search-results-context';
 import { MealType } from '../types';
+import shareBtn from '../images/shareBtn.svg';
+import likeBtn from '../images/likeBtn.svg';
 
 function MainScreenFood() {
   const { id } = useParams<{ id: string }>();
-  const [recipe, setRecipe] = useState<MealType>();
+  const [mealRecipe, setMealRecipe] = useState<MealType>();
+  const [linkCopied, setLinkCopied] = useState(false);
+  const currentUrl = window.location.href;
 
   const fetchRecipe = async () => {
     const response = await fetchApi(`${ID_MEALS_LINK}${id}`);
-    setRecipe(response.meals[0]);
-    console.log(response);
+    setMealRecipe(response.meals[0]);
   };
+
   useEffect(() => {
     fetchRecipe();
   }, []);
 
+  const handleShareBtn = () => {
+    navigator.clipboard.writeText(currentUrl)
+      .then(() => {
+        setLinkCopied(true);
+      })
+      .catch((err) => console.error('Erro ao copiar: ', err));
+  };
+
   return (
     <>
-      <h1 data-testid="recipe-category">{ recipe?.strCategory }</h1>
-      <h2 data-testid="recipe-title">{ recipe?.strMeal }</h2>
+      <nav>
+        <h1 data-testid="recipe-category">{ mealRecipe?.strCategory }</h1>
+        <div>
+          <button
+            data-testid="share-btn"
+            onClick={ () => handleShareBtn() }
+          >
+            { !linkCopied ? <img src={ shareBtn } alt="Botão de Compartilhamento" />
+              : <span>Link copied!</span>}
+          </button>
+          <button
+            data-testid="favorite-btn"
+          >
+            <img src={ likeBtn } alt="Botão de dar Like em uma receita" />
+          </button>
+        </div>
+      </nav>
+      <h2 data-testid="recipe-title">{ mealRecipe?.strMeal }</h2>
       <img
         data-testid="recipe-photo"
-        src={ recipe?.strMealThumb }
-        alt={ recipe?.strMeal }
+        src={ mealRecipe?.strMealThumb }
+        alt={ mealRecipe?.strMeal }
         style={ { maxWidth: '200px', height: 'auto' } }
       />
       <div>
@@ -33,10 +62,10 @@ function MainScreenFood() {
       </div>
       <div>
         <h3>Instructions</h3>
-        <p data-testid="instructions">{ recipe?.strInstructions }</p>
+        <p data-testid="instructions">{ mealRecipe?.strInstructions }</p>
       </div>
       <div>
-        { recipe?.strYoutube ? <iframe title="recipe video" data-testid="video" width="560" height="315" src={ `https://www.youtube.com/embed/${recipe?.strYoutube.split('=')[1]}` } /> : null }
+        { mealRecipe?.strYoutube ? <iframe title="recipe video" data-testid="video" width="560" height="315" src={ `https://www.youtube.com/embed/${mealRecipe?.strYoutube.split('=')[1]}` } /> : null }
       </div>
     </>
   );
