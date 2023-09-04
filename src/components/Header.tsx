527cd8b47269940c14c1e4fbb08b6a222aff718f
Igor Righi <cruigor@gmail.com>
2023-09-04 10:33:42 -0300

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,11 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
+import drinkIcon from '../images/drinkIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
 import SearchBar from './SearchBar';
+import logo from '../images/iconRecipe.png';
+import '../App.css';
 import '../styles/header.css';
 
 type PropType = {
@@ -15,6 +19,9 @@ function Header({ title, iconSearch, iconProfile }: PropType) {
   const [showSideBar, setShowSideBar] = useState(false);
   const showSideBarElement = () => (showSideBar === false
     ? setShowSideBar(true) : setShowSideBar(false));
+
+  const isDrinksPage = title.toUpperCase() === 'DRINKS';
+  const isMealsPage = title.toUpperCase() === 'MEALS';
 
   return (
     <>
